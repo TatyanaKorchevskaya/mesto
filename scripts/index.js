e978bd96cb4137672ecd943376f0b7bcb705fe95
Tatyana Korchevskaya <tvkorchevskaya@gmail.com>
2023-07-03 20:49:54 +0300

    corr2

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,15 @@ const profileInfoEditButton = document.querySelector(
 const popupEdit = document.querySelector('.popup');
 const buttonClosePopupEdit = popupEdit.querySelector('.popup__close');
 const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__input_name');
-const jobInput = formElement.querySelector('.popup__input_job');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile-info__title');
 const profileJob = document.querySelector('.profile-info__intro');
 
 function togglePopup() {
   popupEdit.classList.toggle('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
@@ -19,11 +21,10 @@ function closePopup() {
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  togglePopup();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup();
 }
 formElement.addEventListener('submit', handleFormSubmit);
 profileInfoEditButton.addEventListener('click', togglePopup);
-buttonClosePopupEdit.addEventListener('click', togglePopup);
+buttonClosePopupEdit.addEventListener('click', closePopup);
